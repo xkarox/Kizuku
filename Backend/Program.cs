65b6072d573d96a8770b3bd14eb88774994c9f74
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-04-30 21:21:53 +0200

    setup

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,36 @@
+using Backend.Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration
+                           .GetConnectionString("DatabaseConnection")
+                       ?? throw new InvalidOperationException(
+                           "Connection string 'DatabaseConnection' not found.");
 
+builder.Services.AddDbContext<KizukuContext>(options =>
+    options.UseSqlite($"Data Source={connectionString}"));
+
+// Cookie Authentication
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.ExpireTimeSpan = TimeSpan.FromHours(1);
+        options.SlidingExpiration = true;
+        options.AccessDeniedPath = "/Forbidden";
+        options.LoginPath = "/Login";
+        options.LogoutPath = "/Logout";
+    });
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +41,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
