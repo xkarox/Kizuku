74df3157b6fc5bc3a63b3e2e10cbaf770e108e05
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-05-01 22:50:55 +0200

    add migration

--- Backend/Infrastructure/Migrations/20250501204927_migration_1.Designer.cs
+++ Backend/Infrastructure/Migrations/20250501204927_migration_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(KizukuContext))]
-    partial class KizukuContextModelSnapshot : ModelSnapshot
+    [Migration("20250501204927_migration_1")]
+    partial class migration_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.4");
