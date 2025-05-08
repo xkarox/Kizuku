f22546259eb0c7c65343dc9443a2284a126fd706
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-05-08 15:19:30 +0200

    Setup Frontend environment + component lib

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
+using Sysinfocus.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSysinfocus(jsCssFromCDN: false);
 
 builder.Services.AddScoped(sp => new HttpClient
     { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
