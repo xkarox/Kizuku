0c8e0817af89ed13f4a363e14b391da69a92b38f
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-05-08 22:24:54 +0200

    implement authentication skeleton

---
+++ Frontend/Colors/Colors.cs
@@ -0,0 +1,13 @@
+namespace Frontend.Colors;
+
+public static class Colors
+{
+    internal static string Primary = "#8937DB";
+    internal static string Secondary = "#BC34D9";
+    internal static string PrimarySecondary = Primary + "," + Secondary;
+    internal static string SecondarySecondary = Primary + "," + Secondary;
+
+    internal static string TextLight = "black";
+    internal static string TextDark = "white";
+    internal static string TextDisabled = "grey";
+}
