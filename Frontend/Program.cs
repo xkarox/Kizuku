48738a92c40f185bc6ff0f1436acf21ae50d4222
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-05-13 18:08:47 +0200

    add blazing.mvvm

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,3 +1,4 @@
+using Blazing.Mvvm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
@@ -8,9 +9,9 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Sysinfocus.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMvvm();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 
