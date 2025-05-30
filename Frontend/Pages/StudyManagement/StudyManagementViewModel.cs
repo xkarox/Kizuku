6258aea28ffbe3c5807ae3f8523a6bf0cb51f037
Adrian Grzybek <adrian.grzybek00@gmail.com>
2025-05-30 20:23:27 +0200

    WIP Create Module

--- Frontend/Pages/StudyManagement/StudyManagementViewModel.cs
+++ Frontend/Pages/StudyManagement/StudyManagementViewModel.cs
@@ -1,31 +1,50 @@
 using Blazing.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using Core.Entities;
+using Core.Requests;
 using Frontend.Services.Interfaces;
 using Microsoft.AspNetCore.Components;
 
-namespace Frontend.ViewModels;
+namespace Frontend.Pages.StudyManagement;
 
 public class StudyManagementViewModel
-    (IStudyManagementServiceUI moduleServiceUi) : ViewModelBase
+    (IStudyManagementServiceUI studyManagementService) : ViewModelBase
 {
-    [Parameter]
     public IEnumerable<Module> Modules { get; set; } = Array.Empty<Module>();
     public bool ShowCreateNewModule { get; set; } = false;
     
     public async Task FetchModules()
     {
-        var result = await moduleServiceUi.GetModules();
+        var result = await studyManagementService.GetModules();
         if (result.IsError)
         {
             return;
         }
+
         Modules = result!.Value! ?? Array.Empty<Module>();
     }
 
     public void AddModuleButtonHandler()
     {
         ShowCreateNewModule = true;
+    }
+
+    public async Task<bool> CreateNewModule(string title, string description)
+    {
+        var request = new CreateModuleRequest()
+        {
+            Name = title,
+            Description = description,
+        };
+        var result = await studyManagementService.CreateModule(request);
+        if (result.IsError)
+        {
+            Console.WriteLine("failed to create new module");
+            return false;
+        }
+        
+        await FetchModules();
+        ShowCreateNewModule = false;
+        return true;
     }
     
 }
